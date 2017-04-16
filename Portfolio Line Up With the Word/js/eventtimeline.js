3e68244bc734bcb0f1f00207c0ef8713b4a7147d
Jax-Tsukuyomu <jnetaylor96@gmail.com>
2017-04-16 18:28:44 -0500

    Add files via upload

---
+++ Portfolio Line Up With the Word/js/eventtimeline.js
@@ -0,0 +1,286 @@
+$(document).ready(function(){
+  $("#explain-eight").animate({width: "100%"});
+    $("#eight-info").css({"display":"block"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+
+  $("#eight").click(function(){
+    $("#explain-eight").animate({width: "100%"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"block"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $(".explain").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#nine").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "100%"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"block"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#nine-fifteen").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "100%"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"block"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#nine-thirty").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "100%"});
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"block"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Names of the Workshops*/
+    $("#explain-nine-thirty").css({"display":"flex"});
+    $("#explain-nine-thirty").css({"justify-content":"space-around"});
+    $("#explain-nine-thirty").css({"align-items":"center"});
+    $("#explain-nine-thirty").css({"flex-direction":"column"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#ten-thirty").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "100%"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"block"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#eleven").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "100%"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"block"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#eleven-fifteen").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "100%"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"block"});
+    $("#twelve-fifteen-info").css({"display":"none"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"block", "padding-bottom":"3%"});
+    $("#twelve-fifteen-info-description").css({"display":"none"});
+  })
+  $("#twelve-fifteen").click(function(){
+    $("#explain-eight").animate({width: "0vw"});
+    $("#explain-nine-fifteen").animate({width: "0vw"});
+    $("#explain-nine-thirty").animate({width: "0vw"});
+    $("#explain-ten-thirty").animate({width: "0vw"});
+    $("#explain-eleven").animate({width: "0vw"});
+    $("#explain-eleven-fifteen").animate({width: "0vw"});
+    $("#explain-nine").animate({width: "0vw"});
+    $("#explain-twelve-fifteen").animate({width: "100%"});
+    $("#explain-nine-thirty").css({"display":"none"})
+    /*Appearance of Information*/
+    $("#eight-info").css({"display":"none"});
+    $("#nine-info").css({"display":"none"});
+    $("#nine-fifteen-info").css({"display":"none"});
+    $("#nine-thirty-info").css({"display":"none"});
+    $("#ten-thirty-info").css({"display":"none"});
+    $("#eleven-info").css({"display":"none"});
+    $("#eleven-fifteen-info").css({"display":"none"});
+    $("#twelve-fifteen-info").css({"display":"block"});
+    /*Description of the Events*/
+    $("#eight-info-description").css({"display":"none"});
+    $("#nine-info-description").css({"display":"none"});
+    $("#nine-fifteen-info-description").css({"display":"none"});
+    $("#nine-thirty-info-description").css({"display":"none"});
+    $("#ten-thirty-info-description").css({"display":"none"});
+    $("#eleven-info-description").css({"display":"none"});
+    $("#eleven-fifteen-info-description").css({"display":"none"});
+    $("#twelve-fifteen-info-description").css({"display":"block", "padding-bottom":"3%"});
+  })
+});
